e16e19024e1fe06dc7704c36a85e19e272788832
RishabhBajpai97 <bajpai.rishabh1018@gmail.com>
2024-08-01 22:33:58 +0530

    add base usecase class

---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,6 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:lets_talk/core/error/failure.dart';
+
+abstract interface class Usecase<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
